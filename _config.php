ac59dfda85474a80019e742c790019d171f64d19
marcio.barrientos <marcio.barrientos@unah.edu.hn>
2018-06-06 19:43:41 -0600

    Initial SS4 Support

--- _config.php
+++ _config.php
@@ -1,5 +1,1 @@
 <?php
-
-if (basename(dirname(__FILE__)) != 'editlock') {
-	throw new Exception('The edit lock module is not installed in correct directory. The directory should be named "editlock"');
-}
